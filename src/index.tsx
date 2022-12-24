58f662f1944a749c2fde35eb82366a462f5581a7
globalskye <43073544+globalskye@users.noreply.github.com>
2022-12-25 00:58:59 +0300

    fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CssBaseline } from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import customFont from './fonts/BOYCOTT_.ttf'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Raleway, Arial',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          
+          font-family: 'Raleway';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: local('BOYCOTT'), local('BOYCOTT-Regular'), url(${customFont}) format('woff2');
+          unicodeRange: U+0000-FFFF;
+        }
+      `,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    
+  <ThemeProvider theme={theme}>
+    <CssBaseline/>
+    <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ThemeProvider>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
